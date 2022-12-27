e1f58c77881514d90caa816d0d6f5043612b87ef
mustafawan <121519084+mustafawan@users.noreply.github.com>
2022-12-27 23:33:42 +0300

    Update Heap.java

--- Heap.java
+++ Heap.java
@@ -1,9 +1,31 @@
 
 public class Heap<T> {
 	
-	public void add(int id, T key) {
+		private static int[] heap;
+
+	private int parent(int pos) {
+		return (pos - 1) / 2;
+	}
+
+	private int leftChild(int pos) {
+		return (2 * pos) + 1;
+	}
+
+	private int rightChild(int pos) {
+		return (2 * pos) + 2;
+	}
+
+	public static void createHeap(int size) {
+		heap = new int[size];
+	}
+
+	public void add(int key, T data) {
+
+	}
+
+	public void reheap(int n, int i) {
+		int root = i;
 		
 	}
-	
 
 }
